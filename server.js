03c47801eee3c3d19da83559853c002bcea36171
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2023-11-23 08:21:14 +0530

    unlink complete

--- server.js
+++ server.js
@@ -11,10 +11,19 @@ fs.writeFile('myfile.txt','SAY MY NAME' , (e)=>{
   }
 })
 
-
 fs.readFile('myfile.txt','utf8',(e,d)=>{
    if(e) return;
    console.log(d);
+})
+
+fs.appendFile('myfile.txt','\nGirja',(e)=>{
+  if (e) return;
+  console.log("Appended");
+})
+
+fs.unlink('myfile.txt',(e)=>{
+  if (e) return;
+  console.log("deleted");
 })
 
 
