5ee01b2f3f32c9cc86b4ec51422733fa244e93c5
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2023-12-05 17:29:18 +0530

    added another fetch function

--- secondProcess.js
+++ secondProcess.js
@@ -2,7 +2,16 @@
 
 // Server can talk to server
 
-
 fetch("localhost:3000",{
     method:"Post"
 }).then((result)=>console.log(result));
+
+
+let methodObj={
+    method:"Post"
+}
+
+function callbackFn(result){
+  console.log(result);
+}
+fetch("localhost:3000/calculateSum",methodObj).then(callbackFn)
